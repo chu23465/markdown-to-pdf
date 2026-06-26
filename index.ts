f7511dfc65a13dc9f5e92d9da5d022bae4fcc918
chu23465 <130033130+chu23465@users.noreply.github.com>
2026-06-26 16:40:10 +0530

    Feat: Switch Theme Implemented
    Cleaned up everywhere too

--- index.ts
+++ index.ts
@@ -2,39 +2,124 @@ import {markdownToHTML} from '@nick/comrak';
 
 import.meta.hot.accept();
 
-// I know a lot of this is just cursed. Apologies.
-// This specfic function created with AI
+const rootElement = document.documentElement;
 
-function addPrintStyle(cssString: string) {
+const savedTheme: string | null = localStorage.getItem('theme');
+const systemPrefersDark: boolean = window.matchMedia(
+  '(prefers-color-scheme: dark)',
+).matches;
+const toggleButton = document.getElementById('themeToggle') as HTMLElement;
+
+const stylesArray: StyleSheetList = document.styleSheets as StyleSheetList;
+const divOutput = document.getElementById('markdown-body') as HTMLElement;
+const buttons: HTMLCollection =
+  document.getElementsByClassName('buttonwithCSS');
+
+let hasFile: boolean = false;
+let alerted: boolean = false;
+let printed: boolean = false;
+
+const buttonsThemeLight = new Map<string, string>([
+  ['color', 'rgb(37, 41, 46)'],
+  ['background-color', 'rgb(246, 248, 250)'],
+  ['border-color', 'rgb(209, 217, 224)'],
+  ['box-shadow', 'rgba(31, 35, 40, 0.04) 0px 1px 0px 0px'],
+]);
+const buttonsThemeDark = new Map<string, string>([
+  ['color', 'rgb(240, 246, 252)'],
+  ['background-color', 'rgb(33, 40, 48)'],
+  ['border-color', 'rgb(61, 68, 77)'],
+  ['box-shadow', 'rgba(0, 0, 0, 0) 0px 0px 0px 0px'],
+]);
+
+if (savedTheme) {
+  rootElement.setAttribute('data-theme', savedTheme);
+  switchToTheme(savedTheme);
+} else if (systemPrefersDark) {
+  //Default to Dark Theme if none in storage
+  rootElement.setAttribute('data-theme', 'dark');
+  switchToTheme('dark');
+}
+
+// I know a lot of this is just cursed. Apologies.
+
+function removeLastPrintStyle() {
+  const headNodes: HTMLCollection = document.head.children;
+  for (let i = 0; i < headNodes.length; i++) {
+    if ((headNodes[i] as HTMLElement).localName === 'style') {
+      (headNodes[i] as HTMLElement).remove();
+      //console.log('Removed Style Node');
+    }
+  }
+}
+
+function addStyle(cssString: string, mediaString: string | null) {
   const style = document.createElement('style');
   style.type = 'text/css';
-  style.media = 'print'; // Target print media only
+  if (mediaString) {
+    style.media = mediaString;
+  } // Target print media only
   style.appendChild(document.createTextNode(cssString));
   document.head.appendChild(style);
 }
-const scheme = 'dark';
-const stylesArray: StyleSheetList = document.styleSheets as StyleSheetList;
 
-for (let i = 0; i < stylesArray.length; i++) {
-  for (let j = 0; j < (stylesArray[i] as CSSStyleSheet).rules.length; j++) {
-    if ('media' in ((stylesArray[i] as CSSStyleSheet).rules[j] as CSSRule)) {
-      const tempCSSRule: CSSMediaRule = (stylesArray[i] as CSSStyleSheet).rules[
-        j
-      ] as CSSMediaRule;
-      if (tempCSSRule.conditionText === `(prefers-color-scheme: ${scheme})`) {
-        const themeCSS: string = tempCSSRule.cssText
-          .replace(`(prefers-color-scheme: ${scheme})`, 'print')
-          .replace(`, [data-theme="${scheme}"]`, '');
-        addPrintStyle(themeCSS);
-        break;
+function matchPrintStyle(newTheme: string | null) {
+  for (const i of stylesArray) {
+    for (const j of (i as CSSStyleSheet).rules) {
+      if ('media' in (j as CSSRule)) {
+        const tempCSSRule: CSSMediaRule = j as CSSMediaRule;
+        if (
+          tempCSSRule.conditionText === `(prefers-color-scheme: ${newTheme})`
+        ) {
+          const mediaString: string = 'print';
+          const themeCSS: string = tempCSSRule.cssText
+            .replace(`(prefers-color-scheme: ${newTheme})`, mediaString)
+            .replace(`, [data-theme="${newTheme}"]`, '');
+          removeLastPrintStyle();
+          removeLastPrintStyle();
+          addStyle(themeCSS, mediaString);
+          const forceLightMode = themeCSS
+            .replace('@media print {', '')
+            .replace('; }\n}', '; }');
+          addStyle(forceLightMode, null);
+        }
       }
     }
   }
 }
 
-let hasFile: boolean = false;
-let alerted: boolean = false;
-let printed: boolean = false;
+function switchToTheme(newTheme: string) {
+  //console.log(`Theme set to ${newTheme}`);
+  rootElement.setAttribute('data-theme', newTheme);
+  localStorage.setItem('theme', newTheme);
+  matchPrintStyle(newTheme);
+  if (divOutput instanceof HTMLElement) {
+    const bgColor = window.getComputedStyle(divOutput).backgroundColor;
+    document.documentElement.style.backgroundColor = bgColor;
+  }
+  if (buttons) {
+    for (const i of buttons) {
+      for (const [key, value] of newTheme === 'dark'
+        ? buttonsThemeDark
+        : buttonsThemeLight)
+        (i as HTMLElement).style.setProperty(key as string, value as string);
+    }
+  }
+}
+
+toggleButton.addEventListener('click', event => {
+  (event as Event).preventDefault();
+  const currentTheme = rootElement.getAttribute('data-theme');
+  let newTheme = 'light';
+
+  if (currentTheme === 'light') {
+    newTheme = 'dark';
+  }
+
+  switchToTheme(newTheme);
+});
+
+matchPrintStyle(rootElement.getAttribute('data-theme'));
 
 (
   document.getElementById('pdfGen') as HTMLButtonElement | null
@@ -55,7 +140,8 @@ let printed: boolean = false;
 
 (
   document.getElementById('uploadBtn') as HTMLButtonElement | null
-)?.addEventListener('click', () => {
+)?.addEventListener('click', event => {
+  (event as Event).preventDefault();
   (document.getElementById('fileInput') as HTMLInputElement | null)?.click();
 });
 
@@ -125,13 +211,9 @@ fileInput.addEventListener('change', () => {
           tasklistClasses: true,
         },
       });
-
-      const divOutput = document.getElementById('markdown-body') as HTMLElement;
       //console.log(divOutput);
       if (divOutput instanceof HTMLElement) {
         divOutput.innerHTML = html;
-        const bgColor = window.getComputedStyle(divOutput).backgroundColor;
-        document.documentElement.style.backgroundColor = bgColor;
       }
     };
 
